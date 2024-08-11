2d9c858da3274e19e289b8bc3079bd0a9744cc92
Johnson Chen <chnejohnson@gmail.com>
2024-08-11 18:56:10 +0800

    feat(Options): reverse list

--- src/options/Options.vue
+++ src/options/Options.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import logo from '~/assets/logo.svg'
-import { storageDemo } from '~/logic/storage'
 import { hiddenHouses, savedHouses } from '~/logic/storage'
 
 function removeHiddenHouse(id: string) {
@@ -10,17 +8,36 @@ function removeHiddenHouse(id: string) {
 function unsaveHouse(id: string) {
 	savedHouses.value = savedHouses.value.filter(house => house.id !== id)
 }
+
+const reversedSavedHouses = computed(() => [...savedHouses.value].reverse())
+const reversedHiddenHouses = computed(() => [...hiddenHouses.value].reverse())
 </script>
 
 <template>
-	<main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200 text-base">
+	<main class="px-4 py-10 flex flex-col items-center text-gray-700 dark:text-gray-200 text-base">
 		<!-- <SharedSubtitle /> -->
 
-		<div class="text-xl">Saved Houses</div>
-		<div v-for="house in savedHouses" :key="house.id" class="mt-2">
-			<div class="flex justify-center items-center gap-2">
-				<a :href="`https://rent.591.com.tw/${house.id}`" target="_blank">{{ house.title }}</a>
-				<button class="icon-btn" @click="unsaveHouse(house.id)">Unsave</button>
+		<div class="text-xl">
+			Saved Houses <span>({{ savedHouses.length }})</span>
+		</div>
+		<div class="flex flex-col justify-center">
+			<div v-for="house in reversedSavedHouses" :key="house.id" class="mt-2">
+				<div class="flex gap-2">
+					<button class="ext-btn" @click="unsaveHouse(house.id)">Unsave</button>
+					<a :href="`https://rent.591.com.tw/${house.id}`" target="_blank">{{ house.title }}</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="text-xl mt-10">
+			Hidden Houses <span>({{ hiddenHouses.length }})</span>
+		</div>
+		<div class="flex flex-col justify-center">
+			<div v-for="house in reversedHiddenHouses" :key="house.id" class="mt-2">
+				<div class="flex gap-2">
+					<button class="ext-btn" @click="removeHiddenHouse(house.id)">Remove</button>
+					<a :href="`https://rent.591.com.tw/${house.id}`" target="_blank">{{ house.title }}</a>
+				</div>
 			</div>
 		</div>
 	</main>

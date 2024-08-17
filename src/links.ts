0fc7e5e6f3f80661e3bb04656acd2f1193f2ee44
Johnson Chen <chnejohnson@gmail.com>
2024-08-17 15:14:50 +0800

    fix: update links with new api

--- src/links.ts
+++ src/links.ts
@@ -1,38 +1,38 @@
 export const links = [
 	{
 		name: '整層住家',
-		url: `https://rent.591.com.tw/?
-		showMore=1
+		url: `https://rent.591.com.tw/list?
+		region=1
 		&kind=1
-		&rentprice=10000,30000
+		&price=10000$_30000$
 		&shType=host
 		&floor=3_
-		&multiNotice=not_cover
-		&order=posttime
-		&orderType=desc`,
+		&notice=not_cover
+		&sort=posttime_desc
+		`,
 	},
 	{
 		name: '獨立套房',
-		url: `https://rent.591.com.tw/?
-		showMore=1
+		url: `https://rent.591.com.tw/list?
+		region=1
 		&kind=2
-		&rentprice=9000,15000
+		&price=9000$_15000$
 		&shType=host
 		&floor=3_
-		&multiNotice=not_cover
-		&order=posttime
-		&orderType=desc`,
+		&notice=not_cover
+		&sort=posttime_desc
+		`,
 	},
 	{
 		name: '獨立套房 (1.5-2萬)',
-		url: `https://rent.591.com.tw/?
-		showMore=1
+		url: `https://rent.591.com.tw/list?
+		region=1
 		&kind=2
-		&rentprice=15000,20000
+		&price=15000$_20000$
 		&shType=host
 		&floor=3_
-		&multiNotice=not_cover
-		&order=posttime
-		&orderType=desc`,
+		&notice=not_cover
+		&sort=posttime_desc
+		`,
 	},
 ]
